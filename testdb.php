2965e1bfaf0c808e7b7e52197aa7eba657dae35a
GuyGuiISEN <courmontguillaume@gmail.com>
2015-03-19 19:37:07 +0100

    Page de commande et detail commande

--- testdb.php
+++ testdb.php
@@ -2,15 +2,14 @@
 <html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8"/>
-    <title>Mon premier calculateur</title>
+    <title>Relier Php Et PostgreSql</title>
 </head>
 <body>
-<p><h2>Choisissez UNE colonne et UNE table en rapport</h2></p>
 <form action="testdb.php" method = "post">
 <TABLE BORDER="1"> 
       <TR> 
          <TH> Table </TH> 
-         <TH> Colonne </TH> 
+         <TH> Colonne </TH>
       </TR> 
       <TR> 
          <TD>
@@ -41,7 +40,7 @@
       		<input type="checkbox"  name="select[]" value="reference_commande">Reference Commande</input></br>
       		<input type="checkbox"  name="select[]" value="numero_ligne_commande">Numero Ligne De COmmande</input></br>
       		<input type="checkbox"  name="select[]" value="reference_type_piece">Reference Type Piece</input></br> 
-      		<input type="checkbox"  name="select[]" value="quantites_pieces_commandees">Quantité Pièces Commandées</input></br>
+      		<input type="checkbox"  name="select[]" value="quantite_pieces_commandees">Quantité Pièces Commandées</input></br>
       		<input type="checkbox"  name="select[]" value="prix_piece">Prix Pièce</input></br> 
       	</td>
       </tr>
@@ -74,34 +73,49 @@
       		<input type="checkbox"  name="select[]" value="reference_type_piece">Reference Type Piece</input></br> 
       	</td>
       </tr>
-</TABLE> 
-
-
-<button type="submit" value="envoyer">envoyer</button>
+</TABLE>
+<input type="value" name="Nbr"></input>
+<button type="submit" target="Nbr">Valider</button>
+</br>
 </form>
+<form>
 
-
+<table border="1">
+<tr><th>RESULTAT</th></tr>
+<td>
 <?php
    $db = pg_connect("dbname=cyclesISEN host=localhost user=guillaumecourmont");
    if(!$db){
       echo "Probléme" . "</br>";
-   }else{
-      echo "Entrée" . "</br>";
    }
-   if (isset($_POST['from']) and isset($_POST['select'])) {
- 
+
+   if (isset($_POST['Nbr'])) {
+   	$Nbr = $_POST['Nbr'];
+   }
+
+   if (isset($_POST['from']) and isset($_POST['select']) and isset($Nbr)) {
+ 	
+
    foreach($_POST['from'] as $from){
       foreach ($_POST['select'] as $select) {
          $res = pg_query("SELECT $select FROM $from");
-         while($row =pg_fetch_row($res)){
-            echo "$select : $row[0]" . "</br>";
+         $row = pg_num_rows($res);
+         if ($Nbr>$row) {
+         	$Nbr=$row;
+         }
+         for($i=1;$i<=$Nbr;$i++){
+            $val=pg_fetch_result($res, $i-1, 0);
+            echo $i . " : " .$val."</br>";
          }
       }
    }
-   }
+   pg_free_result($res);
    echo pg_last_error($db);
+   }
 
 ?>
-
+</td>
+</table>
+</form>
 </body>
 </html>
